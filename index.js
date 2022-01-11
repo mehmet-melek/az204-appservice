78a85c33466ca6955ec3df8edefab2d9147f6730
mehmet-melek <mehmetmelekk@gmail.com>
2022-01-11 11:29:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const _ = require('lodash')
+const { json } = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.get('/api/hello', async (req, res) => {
+
+    
+    res.send(JSON.stringify(process.env))
+})
+
+app.get('/api/merhaba', (req, res) => {
+
+    res.send('Merhaba Dünya')
+})
+
+app.post('/api/name', (req, res) => {
+
+    const body = _.pick(req.body, ['firstName','lastName'])
+    console.log(body)
+    res.send('Hello '+body.firstName+' '+body.lastName)
+})
+
+app.listen(8080, () => {
+    console.log('app server is running')
+})
